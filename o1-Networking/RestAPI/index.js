e3dcb4e18a877f06d75f369f3a7b06cd875f59c1
tanktopdev <tanktopdev06@gmail.com>
2026-04-28 13:15:01 +0530

    Added Some CRUD Operations

--- o1-Networking/RestAPI/index.js
+++ o1-Networking/RestAPI/index.js
@@ -1,12 +1,45 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.all('/', (req, res) => {
-    console.log(`request >` , req);
-    console.log(`response >` , res);
-    res.send(`I'M UP!`);
-})
+app.all("/", (req, res) => {
+  // console.log(`request >` , req);
+  // console.log(`response >` , res);
+  res.send(`I'M UP!`);
+});
+
+const todos = [
+  {
+    id: "1",
+    title: "Task 1",
+    completed: false,
+  },
+  {
+    id: "2",
+    title: "Task 2",
+    completed: true,
+  },
+];
+
+//Create
+app.post('/todos', (res, req) => {
+    let newTodos = res.body();
+    todos.push(newTodos);
+    res.json({
+        message: "New Todo Added"
+    })
+});
+
+//Read
+app.get('/todos', (res, req) => {
+    res.json(todos);
+});
+
+//Update
+
+
+//Delete
+
 
 let PORT = 5111;
 app.listen(PORT, () => {
